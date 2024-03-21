bef631a95babf20c276054858dd2bc27996868f3
Sean Eric Thomas <set@seanericthomas.com>
2024-03-21 14:11:46 -0700

    Add input validation to signup

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -43,7 +43,7 @@ export default function Input({
     <div className="relative flex flex-col">
       <label htmlFor={id}>{labelText}</label>
       <input
-        className={`rounded border-2 border-sky-800 p-1 focus:outline focus:outline-2 focus:outline-sky-800 ${attempted ? "invalid:border-red-600 invalid:focus:outline-red-600" : ""}`}
+        className={`rounded border-2 ${attempted && !valid ? "border-red-600 focus:outline-red-600" : "border-sky-800 focus:outline-sky-800"} p-1 focus:outline focus:outline-2 ${attempted ? "invalid:border-red-600 invalid:focus:outline-red-600" : ""}`}
         id={id}
         maxLength={maxLength}
         minLength={minLength}
@@ -54,7 +54,21 @@ export default function Input({
         type={type}
         value={value}
       />
-      {attempted && !valid && message ? <span className="text-sm text-red-600 text-right">{message}</span> : null}
+      {attempted && !valid && message ? (
+        <span className="text-right text-sm text-red-600">{message}</span>
+      ) : null}
+      {pattern ? (
+        <div className="mt-2 rounded bg-sky-100 p-1 text-sm">
+          <span className="font-bold">Password requirements:</span>
+          <ul>
+            <li>8 characters minimum</li>
+            <li>1 uppercase letter</li>
+            <li>1 lowercase letter</li>
+            <li>1 number</li>
+            <li>1 non alphanumeric symbol (!, @, #, etc)</li>
+          </ul>
+        </div>
+      ) : null}
       {attempted ? (
         <span
           className={`absolute right-2 top-8 text-2xl ${valid ? "text-sky-800" : "text-red-600"}`}
--- src/pages/SignUp.tsx
+++ src/pages/SignUp.tsx
@@ -8,13 +8,21 @@ import Form from "@components/Form";
 
 import { UserContext } from "@contexts/Users";
 
+import Response from "@interfaces/Response";
+
 export default function SignUp() {
   const auth = useContext(UserContext);
   const { attemptSignup } = auth;
 
   const navigate = useNavigate();
 
+  const [attempted, setAttempted] = useState(false);
   const [error, setError] = useState<string | null>(null);
+  const defaultMessages = {
+    email: "Valid email required, 255 characters max",
+    username: "Username required, 3-20 characters",
+  };
+  const [messages, setMessages] = useState(defaultMessages);
 
   const defaultFormInfo = {
     username: {
@@ -55,6 +63,12 @@ export default function SignUp() {
             },
           };
         });
+        setMessages((prevState) => {
+          return {
+            ...prevState,
+            username: "Username required, 3-20 characters",
+          };
+        });
         break;
       case "password":
         setFormInfo((prevState) => {
@@ -88,6 +102,12 @@ export default function SignUp() {
             },
           };
         });
+        setMessages((prevState) => {
+          return {
+            ...prevState,
+            email: "Valid email required, 255 characters max",
+          };
+        });
         break;
       case "name":
         setFormInfo((prevState) => {
@@ -108,12 +128,43 @@ export default function SignUp() {
   // our "confirm" field is only valid if it matches the password
   const handleConfirmValidity = (event: React.SyntheticEvent) => {
     const target = event.target as HTMLInputElement;
-    if (target.value === formInfo.password.value) {
-      target.setCustomValidity("");
+    if (target.id === "confirm") {
+      // we're in the "confirm" input, simple custom validity
+      if (target.value === formInfo.password.value) {
+        target.setCustomValidity("");
+      } else {
+        target.setCustomValidity("Passwords do not match");
+      }
+      target.reportValidity();
     } else {
-      target.setCustomValidity("Passwords do not match");
+      // we're in the "password" field, need to update "confirm" field validity
+      const confirmInput = document.querySelector(
+        "#confirm",
+      ) as HTMLInputElement;
+      if (target.value === confirmInput.value) {
+        confirmInput.setCustomValidity("");
+        setFormInfo((prevState) => {
+          return {
+            ...prevState,
+            confirm: {
+              value: confirmInput.value,
+              valid: true,
+            },
+          };
+        });
+      } else {
+        confirmInput.setCustomValidity("Passwords do not match");
+        setFormInfo((prevState) => {
+          return {
+            ...prevState,
+            confirm: {
+              value: confirmInput.value,
+              valid: false,
+            },
+          };
+        });
+      }
     }
-    target.reportValidity();
   };
 
   // prevent the custom validity error message from popping up
@@ -127,26 +178,122 @@ export default function SignUp() {
     true,
   );
 
-  const submit = async () => {
-    const result = await attemptSignup({
-      username: formInfo.username.value,
-      password: formInfo.password.value,
-      email: formInfo.email.value,
-      name: formInfo.name.value,
-    });
-    if (result.status === 201) {
-      // success, redirect to landing page
-      navigate("/");
+  const checkAllValid = () => {
+    return (
+      formInfo.username.valid &&
+      formInfo.password.valid &&
+      formInfo.confirm.valid &&
+      formInfo.email.valid &&
+      formInfo.name.valid
+    );
+  };
+
+  const checkAlreadyTaken = (response: Response) => {
+    const { errors } = response;
+    if (!errors) {
+      return null;
+    }
+    return {
+      email: errors.email ? errors.email.msg === "Email already in use" : false,
+      username: errors.username
+        ? errors.username.msg === "Username already taken"
+        : false,
+    };
+  };
+
+  const handleAlreadyTaken = (alreadyTaken: {
+    email: boolean;
+    username: boolean;
+  }) => {
+    if (alreadyTaken.email && !alreadyTaken.username) {
+      setError("Email already in use");
+      setFormInfo((prevState) => {
+        return {
+          ...prevState,
+          email: {
+            value: formInfo.email.value,
+            valid: false,
+          },
+        };
+      });
+      setMessages((prevState) => {
+        return {
+          ...prevState,
+          email: "Email already in use",
+        };
+      });
+    } else if (!alreadyTaken.email && alreadyTaken.username) {
+      setError("Username already taken");
+      setFormInfo((prevState) => {
+        return {
+          ...prevState,
+          username: {
+            value: formInfo.username.value,
+            valid: false,
+          },
+        };
+      });
+      setMessages((prevState) => {
+        return {
+          ...prevState,
+          username: "Username already taken",
+        };
+      });
     } else {
-      // XXX
-      // need to parse error messages & provide feedback to user
-      console.error(result);
-      setError(result.message);
+      setError("Email & username already in use");
+      setFormInfo((prevState) => {
+        return {
+          ...prevState,
+          email: {
+            value: formInfo.email.value,
+            valid: false,
+          },
+          username: {
+            value: formInfo.username.value,
+            valid: false,
+          },
+        };
+      });
+      setMessages({
+        email: "Email already in use",
+        username: "Username already taken",
+      });
+    }
+  };
+
+  const submit = async () => {
+    setAttempted(true);
+    if (!checkAllValid()) {
+      setError("Invalid input(s) - check each field");
+    } else {
+      const result = await attemptSignup({
+        username: formInfo.username.value,
+        password: formInfo.password.value,
+        email: formInfo.email.value,
+        name: formInfo.name.value,
+      });
+      if (result.status === 201) {
+        // success, redirect to landing page
+        navigate("/");
+      } else if (result.status === 400) {
+        const alreadyTaken = checkAlreadyTaken(result);
+        if (alreadyTaken) {
+          handleAlreadyTaken(alreadyTaken);
+        } else {
+          console.error(result);
+          setError(result.message);
+        }
+      } else {
+        // XXX
+        // need to parse error messages & provide feedback to user
+        console.error(result);
+        setError(result.message);
+      }
     }
   };
 
   return (
-    <div className="p-2">
+    <div className="flex flex-col gap-4 p-2">
       <div className="rounded bg-white shadow-md shadow-slate-400">
         <h1 className="rounded-t bg-sky-600 p-1 text-2xl text-neutral-100">
           Sign up
@@ -154,61 +301,78 @@ export default function SignUp() {
         <div className="flex flex-col gap-4 p-1">
           <Form>
             <Input
+              attempted={attempted}
               id="username"
               labelText="username:"
+              message={messages.username}
               maxLength={20}
               minLength={3}
               onChange={handleChange}
               required
               type="text"
+              valid={formInfo.username.valid}
               value={formInfo.username.value || ""}
             />
 
             <Input
+              attempted={attempted}
               id="password"
               labelText="password:"
-              onChange={handleChange}
+              message="Password doesn't meet requirements"
+              onChange={(event) => {
+                handleChange(event);
+                handleConfirmValidity(event);
+              }}
               pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^a-zA-Z0-9]).{8,}$"
               required
               title="8 characters minimum, 1 uppercase letter, 1 lowercase letter, 1 number, 1 symbol"
               type="password"
+              valid={formInfo.password.valid}
               value={formInfo.password.value || ""}
             />
 
             <Input
+              attempted={attempted}
               id="confirm"
               labelText="confirm password:"
+              message="Passwords do not match"
               onChange={(event) => {
                 handleChange(event);
                 handleConfirmValidity(event);
               }}
               required
               type="password"
+              valid={formInfo.confirm.valid}
               value={formInfo.confirm.value || ""}
             />
 
             <Input
+              attempted={attempted}
               id="email"
               labelText="email:"
               maxLength={255}
+              message={messages.email}
               onChange={handleChange}
               required
               type="email"
+              valid={formInfo.email.valid}
               value={formInfo.email.value || ""}
             />
 
             <Input
+              attempted={attempted}
               id="name"
               labelText="name:"
               maxLength={255}
+              message="Name required, 255 characters max"
               onChange={handleChange}
               required
               type="text"
+              valid={formInfo.name.valid}
               value={formInfo.name.value || ""}
             />
 
             <Button onClick={submit}>Submit</Button>
-            <ErrorMessage error={error} />
           </Form>
           <div className="text-lg">
             <span>Already have an account? </span>
@@ -219,6 +383,7 @@ export default function SignUp() {
           </div>
         </div>
       </div>
+      <ErrorMessage error={error} />
     </div>
   );
 }
