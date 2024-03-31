a7d38961227c6cf282947e9a923df53fc76f57b3
Sean Eric Thomas <set@seanericthomas.com>
2024-03-31 10:16:16 -0700

    Display mod-specific comment controls

--- src/components/CommentControl.tsx
+++ src/components/CommentControl.tsx
@@ -11,21 +11,27 @@ import groups from "@util/groups";
 interface Props {
   author: string;
   commentByAdmin: boolean;
+  commentByMod: boolean;
   commentId: string;
   eligibleForBan: boolean;
   groupId: string;
+  isOwnComment: boolean;
   postId: string;
   updateComments: () => void;
+  userIsAdmin: boolean;
 }
 
 export default function CommentControl({
   author,
   commentByAdmin,
+  commentByMod,
   commentId,
   eligibleForBan,
   groupId,
+  isOwnComment,
   postId,
   updateComments,
+  userIsAdmin,
 }: Props) {
   const [banUser, setBanUser] = useState(false);
   const [deleteComment, setDeleteComment] = useState(false);
@@ -64,6 +70,27 @@ export default function CommentControl({
     }
   };
 
+  const showDeleteComment = () => {
+    // admin can delete any comment, mods cannot delete comments by admin or other mods
+    if (!userIsAdmin && (commentByAdmin || (commentByMod && !isOwnComment))) {
+      return false;
+    }
+    return true;
+  };
+
+  const showBanUser = () => {
+    // first check if their eleigible for a ban or if its an admin's comment
+    if (!eligibleForBan || commentByAdmin) {
+      return false;
+    }
+    // now, if they're a mod they cannot ban themselves or other mods
+    if (!userIsAdmin && commentByMod) {
+      return false;
+    }
+    // otherwise show the option
+    return true;
+  };
+
   return (
     <div className="rounded border-2 border-orange-600 bg-white">
       <h2 className="bg-orange-600 p-1 text-2xl capitalize text-neutral-100">
@@ -71,13 +98,15 @@ export default function CommentControl({
       </h2>
       <div className="flex flex-col gap-2 p-1">
         <Form>
+          {showDeleteComment() ?
           <Checkbox
             checkedState={deleteComment}
             id={`deletecomment${commentId}`}
             onChange={toggleDelete}
             labelText="delete comment"
           />
-          {!commentByAdmin && eligibleForBan ? (
+          : null}
+          {showBanUser() ? (
             <Checkbox
               checkedState={banUser}
               id={`banuser${commentId}`}
--- src/components/CommentDetail.tsx
+++ src/components/CommentDetail.tsx
@@ -24,12 +24,27 @@ export default function CommentDetail({ comment, group, postId, updateComments }
   }
 
   // this detemrines if the PostControl component is rendered
-  const isAdmin = user.id === group.admin;
+  const userIsAdmin = user.id === group.admin;
+  const userIsMod = group.mods.includes(user.id);
 
   // these will determine if the "ban user" checkbox shows up in PostControl
   const eligibleForBan =
     !group.banned.includes(comment.author) && group.members.includes(comment.author);
   const commentByAdmin = comment.author === group.admin;
+
+  // these will determine what comments a mod can or cannot delete
+  const commentByMod = group.mods.includes(comment.author);
+  const isOwnComment = user.id === comment.author;
+
+  const showCommentControl = () => {
+    if (!userIsAdmin && !userIsMod) {
+      return false;
+    }
+    if (userIsMod && (commentByAdmin || (commentByMod && !isOwnComment))) {
+      return false;
+    }
+    return true;
+  };
 
   return (
     <li className="rounded bg-white shadow-md shadow-slate-400">
@@ -37,15 +52,18 @@ export default function CommentDetail({ comment, group, postId, updateComments }
         <Username userId={comment.author} />
       </div>
       <div className="flex flex-col gap-4 p-1">
-        {isAdmin ? (
+        {showCommentControl() ? (
           <CommentControl
             author={comment.author}
             commentByAdmin={commentByAdmin}
             commentId={comment.id}
+            commentByMod={commentByMod}
             eligibleForBan={eligibleForBan}
             groupId={group.id}
+            isOwnComment={isOwnComment}
             postId={postId}
             updateComments={updateComments}
+            userIsAdmin={userIsAdmin}
           />
         ) : null}
         <pre className="whitespace-pre-wrap font-sans text-lg">
